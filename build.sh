e792486a2d883ea3f9c6add2d8ec87cb77c9e918
Rich Tasker <ritasker8t2@gmail.com>
2020-05-21 22:15:10 +0100

    copy deploy to artifacts

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ dotnet build -c Release
 dotnet pack -o $PWD/artifacts
 
 grep '<Version>' < ./src/Pier8.DbTools/*.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/' > $PWD/artifacts/version.txt
+cp deploy.sh $PWD/artifacts/
