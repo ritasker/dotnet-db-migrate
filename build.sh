1fb504487c11ab78b24a56d404760c93bfdf1919
Richard Tasker <ritasker8t2@gmail.com>
2021-11-09 22:35:33 +0000

    Include README in .nupkg

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-dotnet restore
-dotnet build -c Release
-dotnet pack -o $PWD/artifacts
-
-grep '<Version>' < ./src/Pier8.DbTools/*.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/' > $PWD/artifacts/version.txt
-cp deploy.sh $PWD/artifacts/
